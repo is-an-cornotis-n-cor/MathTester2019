8066f12f08d279ca5b52e9d1d925f79c99a8af9b
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-09-12 10:20:09 -0400

    Added" I will ask Mr. Walters" to main method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,7 +6,9 @@ public class Main {
 	System.out.println("Hello Source Control");
 	System.out.println("And welcome to my java file");
 	System.out.println("We will be doing something today");
-	System.out.println("But i do not know what we will be doing");`
+	System.out.println("But i do not know what we will be doing");
+	System.out.println("I will ask Mr. Walters");
+
 
     }
 }
