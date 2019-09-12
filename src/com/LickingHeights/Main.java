d1d04e6922da1460f39c3bfc8e9a1821876e932b
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-09-12 10:16:40 -0400

    Added "But i do not know what we will be doing"

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	System.out.println("Hello Source Control");
 	System.out.println("And welcome to my java file");
 	System.out.println("We will be doing something today");
+	System.out.println("But i do not know what we will be doing");`
+
     }
 }
