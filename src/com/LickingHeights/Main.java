dfa09fc1be64eab4d2bc1cc03168e9026d2bb929
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-09-12 10:12:09 -0400

    Added "We will be doing something today" to main method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello Source Control");
 	System.out.println("And welcome to my java file");
+	System.out.println("We will be doing something today");
     }
 }
