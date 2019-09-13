ab939d8f812548e14062b7f830a48fbd6e668196
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-09-13 09:44:41 -0400

    reset main page

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,12 +3,7 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello Source Control");
-	System.out.println("And welcome to my java file");
-	System.out.println("We will be doing something today");
-	System.out.println("But i do not know what we will be doing");
-	System.out.println("I will ask Mr. Walters");
-	System.out.println("I found out what we are going to do today");
+	
 
 
     }
