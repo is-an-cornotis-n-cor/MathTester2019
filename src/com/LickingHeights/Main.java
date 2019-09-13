af136a4c22015251a325b9ca6b960b92d6eb2e81
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-09-13 09:55:32 -0400

    Added "First Word plus Second Word" to main method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,7 +3,21 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	
+	//declare
+        String firstWord;
+        String secondWord;
+        int firstNumber,secondNumber;
+
+        //initializing
+        firstWord = "dog";
+        secondWord = "house";
+        firstNumber = 5;
+        secondNumber = 10;
+
+        System.out.println(firstWord+secondWord);
+
+
+
 
 
     }
